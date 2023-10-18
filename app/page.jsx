3a85c5ba0abe2c73e105e34d5fa52efb3fd8157a
MathildaBoullenger <mathilda.boullenger@outlook.com>
2023-10-18 19:46:07 +1300

    trying to use next.js image

--- app/page.jsx
+++ app/page.jsx
@@ -4,48 +4,73 @@ import DisplayFeaturedProject from "@/components/DisplayFeaturedProject";
 import { getFeaturedProject } from "@/lib/projects";
 import Service from "@/components/Service";
 import Link from "next/link";
+import Image from "next/image";
 
 export default async function HomePage() {
-
   const project = await getFeaturedProject();
-    
-    return (
-        <>
-        <Heading delay={0}>Welcome!</Heading>
-        <Heading delay={250}>I'm Esperanza - a 3D Designer</Heading>
-        <Heading delay={500}> based in Auckland, New Zealand.</Heading>
 
-        <div className="mt-2 laptop:mt-5 flex flex-wrap mob:flex-nowrap link">
+  return (
+    <>
+      <Heading delay={0}>Welcome!</Heading>
+      <Heading delay={250}>I'm Esperanza - a 3D Designer</Heading>
+      <Heading delay={500}> based in Auckland, New Zealand.</Heading>
 
-        <Link href="https://www.linkedin.com/in/esperanzalbani/" target="_blank">
-        <Button delay={750}> <img src="/images/LI-Logo-white.png" className="h-5"/></Button>
+      <div className="mt-2 laptop:mt-5 flex flex-wrap mob:flex-nowrap link">
+        <Link
+          href="https://www.linkedin.com/in/esperanzalbani/"
+          target="_blank"
+        >
+          <Button delay={750}>
+            <Image
+              src="/LI-Logo-white.png" // Path to your image in the public directory
+              alt="LinkedIn Logo"
+              width={50} // Specify the desired width
+              height={20} // Specify the desired height
+              className="h-5"
+            />
+          </Button>
         </Link>
-
+        In the Image component, you specify the path to your image, its alt
+        text, and the desired width and height attributes. You can adjust the
+        width and height values according to your design requirements. Make sure
+        to replace /LI-Logo-white.png with the actual path to your image in the
+        public directory. This will ensure that the image is properly optimized
+        and served by Next.js.
         <Link href="mailto:esperanzalbani@gmail.com" target="_blank">
-        <Button delay={750}><img src="/images/Email1.png" className="h-5"/></Button>
+          <Button delay={750}>
+            <img src="/images/Email1.png" className="h-5" />
+          </Button>
         </Link>
-        </div>   
-        
-{/* project section */}
+      </div>
 
-        <DisplayFeaturedProject delay={1500} project={project}></DisplayFeaturedProject>
+      {/* project section */}
 
+      <DisplayFeaturedProject
+        delay={1500}
+        project={project}
+      ></DisplayFeaturedProject>
 
-{/* services section */}
+      {/* services section */}
 
-<div className="mt-16 mb-16 laptop:mt-30 p-2 laptop:p-0">
-  <Heading delay={1750}>Services.</Heading>
-<div className="mt-5 tablet:m-10 grid grid-cols-1 laptop:grid-cols-2 gap-6">
-    
-<Service delay={2000} title={'Renders'} content={'This is my best skill!'}></Service>
-<Service delay={2000} title={'3D Design'} content={'This is my second best skill!'}></Service>
+      <div className="mt-16 mb-16 laptop:mt-30 p-2 laptop:p-0">
+        <Heading delay={1750}>Services.</Heading>
+        <div className="mt-5 tablet:m-10 grid grid-cols-1 laptop:grid-cols-2 gap-6">
+          <Service
+            delay={2000}
+            title={"Renders"}
+            content={"This is my best skill!"}
+          ></Service>
+          <Service
+            delay={2000}
+            title={"3D Design"}
+            content={"This is my second best skill!"}
+          ></Service>
+        </div>
+      </div>
 
-  </div>
-</div>
+      {/* Continue adding more project blocks here */}
 
-  {/* Continue adding more project blocks here */}
-
-            {/*<div classNameName="bg-white border rounded shadow w-80 hover:shadow-xl sm:w-full">
+      {/*<div classNameName="bg-white border rounded shadow w-80 hover:shadow-xl sm:w-full">
           <Link href={`/projects/${project.slug}`}
           classNameName="flex flex-col sm:flex-row">
             <img
@@ -59,8 +84,6 @@ export default async function HomePage() {
             </h2>
           </Link>
     </div>*/}
-
-
-        </>
-    );
+    </>
+  );
 }
