623741877592f11dd0775178af43c0405ec05457
MathildaBoullenger <mathilda.boullenger@outlook.com>
2023-10-20 16:54:13 +1300

    slider

--- components/ProjectsCarousel.jsx
+++ components/ProjectsCarousel.jsx
@@ -18,7 +18,7 @@ export default function ProjectsCarousel({ projects, delay }) {
   }, [delay]);
 
   return (
-    <div id="default-carousel" data-carousel="slide" className={`relative w-full mt-16 ${isVisible ? '' : 'opacity-0'}`}>
+    <div id="default-carousel" data-carousel="static" className={`relative w-full mt-16 ${isVisible ? '' : 'opacity-0'}`}>
       {projects.map((project, index) => (
         <div key={index} className="relative h-56 overflow-hidden rounded-lg md:h-96">
           {project.images && project.images.length > 0 ? (
@@ -37,6 +37,14 @@ export default function ProjectsCarousel({ projects, delay }) {
           ) : null}
         </div>
       ))}
+
+<div class="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
+        <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
+        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
+        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
+        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
+        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
+    </div>
 
       <button type="button" className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
         <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
