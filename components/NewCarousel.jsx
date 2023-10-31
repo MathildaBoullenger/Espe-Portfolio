da4ae943ba9a9649216e6d0dd45cd52d571bca09
MathildaBoullenger <mathilda.boullenger@outlook.com>
2023-10-31 19:16:37 +1300

    Carousel without bootstrap because css conflicting

--- components/NewCarousel.jsx
+++ components/NewCarousel.jsx
@@ -1,12 +1,9 @@
 "use client"
-
 import React, { useState, useEffect } from 'react';
-import { Carousel } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function NewCarousel({ project, delay }) {
   const [isVisible, setIsVisible] = useState(false);
-  const [index, setIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -16,32 +13,71 @@ export default function NewCarousel({ project, delay }) {
     return () => clearTimeout(timer);
   }, [delay]);
 
-
-  const handleSelect = (selectedIndex) => {
-    setIndex(selectedIndex);
+  const nextSlide = () => {
+    setCurrentIndex((currentIndex + 1) % project.images.length);
   };
 
-  return (
-    <div
-    className={`${
-      isVisible ? "animate-fade-left" : "opacity-0"
-    }`}>
-    <Carousel activeIndex={index} onSelect={handleSelect}>
-      {project.images.map((image, idx) => (
-        <Carousel.Item key={idx}>
-          <img
-            src={`/Espe-Portfolio/${image}`}
-            alt="Slide"
-            className="rounded-lg mt-8"
-          />
-          <Carousel.Caption>
-            {/*<h3>Title</h3>
-            <p>Body</p>*/}
-          </Carousel.Caption>
-        </Carousel.Item>
-        
-      ))}
-    </Carousel>
-    </div>
-  );
-}
+  const prevSlide = () => {
+    setCurrentIndex((currentIndex - 1 + project.images.length) % project.images.length);
+  };
+
+    return (
+      <div
+        className={`carousel-container ${
+          isVisible ? "animate-fade-left" : "opacity-0"
+        }`}
+      >
+        <div className="carousel">
+          {project.images.map((image, idx) => (
+            <div
+              key={idx}
+              className={`carousel-item ${
+                idx === currentIndex ? "active" : "hidden"
+              }`}
+            >
+              <img
+                src={`/Espe-Portfolio/${image}`}
+                alt="Slide"
+                className="rounded-lg mt-8"
+              />
+              <button className="carousel-control prev" onClick={prevSlide}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-8 h-8"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15.75 19.5L8.25 12l7.5-7.5"
+                  />
+                </svg>
+              </button>
+              <button className="carousel-control next" onClick={nextSlide}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-8 h-8"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M8.25 4.5l7.5 7.5-7.5 7.5"
+                  />
+                </svg>
+              </button>
+              <div className="carousel-caption">
+                {/* Your caption content here */}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
